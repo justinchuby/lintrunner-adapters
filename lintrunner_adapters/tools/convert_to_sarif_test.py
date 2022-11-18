56ff78dd29c59756d3b7b80c2b95cc943cfd2fda
Justin Chu <justinchuby@users.noreply.github.com>
2022-11-18 09:11:44 -0800

    format

--- lintrunner_adapters/tools/convert_to_sarif_test.py
+++ lintrunner_adapters/tools/convert_to_sarif_test.py
@@ -47,9 +47,7 @@ class TestConvertToSarif(unittest.TestCase):
                                 {
                                     "id": "FLAKE8/test-code",
                                     "name": "FLAKE8/test-code",
-                                    "shortDescription": {
-                                        "text": "FLAKE8/test-code"
-                                    },
+                                    "shortDescription": {"text": "FLAKE8/test-code"},
                                     "fullDescription": {
                                         "text": "FLAKE8/test-code\ntest description"
                                     },
@@ -58,9 +56,7 @@ class TestConvertToSarif(unittest.TestCase):
                                 {
                                     "id": "FLAKE8/test-code-2",
                                     "name": "FLAKE8/test-code-2",
-                                    "shortDescription": {
-                                        "text": "FLAKE8/test-code-2"
-                                    },
+                                    "shortDescription": {"text": "FLAKE8/test-code-2"},
                                     "fullDescription": {
                                         "text": "FLAKE8/test-code-2\ntest description"
                                     },
