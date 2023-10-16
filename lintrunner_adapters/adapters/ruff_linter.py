027635d1fdb7264d16b1cf6d348892f85d629169
Justin Chu <justinchuby@users.noreply.github.com>
2023-10-16 16:39:32 -0700

    Update ruff_linter.py

--- lintrunner_adapters/adapters/ruff_linter.py
+++ lintrunner_adapters/adapters/ruff_linter.py
@@ -101,7 +101,7 @@ def check_files(
                 "ruff",
                 "--exit-zero",
                 "--quiet",
-                "--format=json",
+                "--output-format=json",
                 *([f"--config={config}"] if config else []),
                 *filenames,
             ],
